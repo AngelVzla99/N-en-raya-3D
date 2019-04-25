63c2be586e118dfbc39b4da5636391682314d735
AngelVzla99 <45955820+AngelVzla99@users.noreply.github.com>
2019-04-25 01:17:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ fila = 0
 columna = 0
 turno = 0
 
+class jugador:
+    def __init__(self):
+    	self.filas = 0
+    	self.columnas = 0
+    	self.diagonales = 0
+    	self.enZ = 0
+
+
 def quedanFichas(T: [[[int]]]) -> bool:
 	hayFichas = any( any( any(T[i][j][k]==0 for k in range(0, N)) for j in range(0, N)) for i in range(0, N) )
 	return hayFichas
@@ -22,9 +30,21 @@ def hayLineaDiagonal(T: [[int]], tablero: int , fila: int, columna: int, turno: 
 def hayLineaDiagonalInversa(T: [[int]], tablero: int , fila: int, columna: int, turno: int) -> bool:
 def hayLineaEnZ(T: [[[int]]], fila: int, columna: int, turno: int) -> bool:
 
-def hayLinea(): 
+def hayLinea(T, N, jugador, fila, columna, tablero):
+	if hayLineaHorizontal(T,Tablero,fila,jug):
+		jugador.filas += 1
+	if hayLineaVertical(T,Tablero,fila,jug):
+		jugador.columnas += 1
+	if fila == columna:
+		if hayLineaDiagonal(T,Tablero,jug):
+			jugador.diagonales += 1
+	if fila + columna == N-1:
+		if hayLineaDiagonalInversa(T,Tablero,jug):
+			jugador.diagonales += 1
+	if 
+
 
 def pedirJugada():
 
 if hayLinea():
-	sumarLineas()
+sumarLineas()
